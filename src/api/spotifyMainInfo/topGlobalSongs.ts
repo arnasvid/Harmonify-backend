e5129c8e09d3d779e9ecfe19ed955f593b387146
arnasvid <arnas.vidziunas@gmail.com>
2023-05-22 00:03:27 +0300

    api for new releases

--- src/api/spotifyMainInfo/topGlobalSongs.ts
+++ src/api/spotifyMainInfo/topGlobalSongs.ts
@@ -13,7 +13,7 @@ const client_id = process.env.CLIENT_ID;
 app.get("/topGlobalSongs", async (req: Request, res: Response) => {
   const playlistId = req.params.playlistId;
   let accessToken = await RequestAccessToken();
-  console.log("BRUH: ",accessToken);
+  // console.log("BRUH: ",accessToken);
   try {
     // const response = await axios.get(
     //   `https://api.spotify.com/v1/playlists/${playlistId}`,
@@ -43,5 +43,29 @@ app.get("/topGlobalSongs", async (req: Request, res: Response) => {
     res.status(500).send("Error retrieving playlist");
   }
 });
+
+app.get("/new-releases", async (req: Request, res: Response) => {
+  let accessToken = await RequestAccessToken();
+  try {
+    const response = await fetch(
+      "https://api.spotify.com/v1/browse/new-releases?limit=50&offset=0",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Bearer " + accessToken.access_token,
+      },
+    }
+    ).then((response) => response.json());
+
+    console.log("response: ",response);
+
+    res.json(response);
+  } catch (error) {
+    console.error("Error retrieving new releases:", error)
+    res.status(500).send("Error retrieving new releases");
+  }
+});
+    
 
 export default app;
